8dfe20e797447ca3b28aba1292742c9d0b8bd0b5
serge089 <sergelegaspi15@gmail.com>
2026-01-23 03:40:55 +0800

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,6 +7,14 @@ import StyleContext from "../../contexts/StyleContext";
 export default function Footer() {
   const {isDark} = useContext(StyleContext);
   return (
-    
+        <Fade bottom duration={1000} distance="5px">
+      <div className="footer-div">
+        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+          {emoji("Made with ❤️ by Serge Team")}
+        </p>
+          </a>
+        </p>
+      </div>
+    </Fade>
   );
 }
